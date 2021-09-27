d11942c514b60a6d6789c8f57ac2bfa4b5b71512
Sebastian M. Nicolajsen <sebastian.m.nicolajsen@gmail.com>
2021-09-27 16:38:39 +0200

    added initial run for filters

--- src/loader.js
+++ src/loader.js
@@ -5,6 +5,8 @@ import filter from './filter.js'
 export default async function loader(input) {
     let result = input;
     let process_tag = "initial";
+    result = filter(result, filters, process_tag); // pre filter
+    
     for(const [_, handle] of Object.entries(handles)){
         const output = await handle(result)
         result = output.result
